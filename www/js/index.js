08fe21f06012268fcbfef1681e6038c01c42ba7f
Tony Malins <tony.malins@contractcandles.com>
2014-04-01 12:38:14 +0100

    New

--- www/js/index.js
+++ www/js/index.js
@@ -41,32 +41,61 @@ var app = {
 		var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
 		var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 		
+		var tickTime = 1000;
+		//Start counting
+		setInterval( function() {
 		// Create a newDate() object
 		var newDate = new Date();
+		var currentTime = newDate;
+		
 		// Extract the current date from Date object
 		newDate.setDate(newDate.getDate());
-		// Output the day, date, month and year   
-		$('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
 		
-		setInterval( function() {
-			// Create a newDate() object and extract the seconds of the current time on the visitor's
-			var seconds = new Date().getSeconds();
-			// Add a leading zero to seconds value
-			$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
-			},1000);
+		// Output the day, date, month and year   
+		$('.date').html(
+		dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear()
+		);
+		
+		//setInterval( function() {
 			
-		setInterval( function() {
-			// Create a newDate() object and extract the minutes of the current time on the visitor's
-			var minutes = new Date().getMinutes();
-			// Add a leading zero to the minutes value
-			$("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
-			},1000);
+				//var milliseconds = currentTime.getMilliseconds();
+				// Add a leading zero to seconds value
+				//$("#millisec").html(Math.round(( milliseconds < 10 ? "0" : "" ) + milliseconds / 10));
+				//// Create a newDate() object and extract the seconds of the current time on the visitor's
+				////var seconds = new Date().getSeconds();
+				//var seconds = currentTime.getSeconds();
+				// Add a leading zero to seconds value
+				//$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+			//},1000);
 			
-		setInterval( function() {
-			// Create a newDate() object and extract the hours of the current time on the visitor's
-			var hours = new Date().getHours();
-			// Add a leading zero to the hours value
-			$("#hours").html(( hours < 10 ? "0" : "" ) + hours);
-			}, 1000);	
-			}
+		//setInterval( function() {
+				// Create a newDate() object and extract the minutes of the current time on the visitor's
+				//var minutes = new Date().getMinutes();
+				var minutes = currentTime.getMinutes();
+				// Add a leading zero to the minutes value
+				$(".min").html(( minutes < 10 ? "0" : "" ) + minutes);
+			//},1000);
+			
+		//setInterval( function() {
+				// Create a newDate() object and extract the hours of the current time on the visitor's
+				//var hours = new Date().getHours();
+				var hours = currentTime.getHours();
+				
+				// Convert the hours component to 12-hour format if needed
+				hours = ( hours > 12 ? hours - 12 : hours );
+				// Convert an hours component of "0" to "12"
+				hours = ( hours == 0 ? 12 : hours );
+				
+				// Add a leading zero to the hours value
+				$(".hours").html(( hours < 10 ? "0" : "" ) + hours );
+				
+				
+				$(".12hr").html(( hours < 12 ? "AM" : "PM" ));
+				
+			}, tickTime);
+			
+			///////////////////////////////////////////////////////////////////
+			
+				
+		}
 };
